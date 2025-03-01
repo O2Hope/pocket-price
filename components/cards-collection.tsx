3705e91f24d2c344c542f819892c75dd94c2a5d1
Agustin Ruiz <Agustin.Ruiz+EYGS@ey.com>
2025-02-28 19:31:07 -0600

    feat: add motion to cards collection

--- components/cards-collection.tsx
+++ components/cards-collection.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AnimatePresence, motion } from "motion/react";
 import { Input } from "./ui/input";
 import PokemonCard from "./ui/pokemon-card";
 
@@ -26,23 +27,49 @@ export const CardsCollection = ({ cards }: { cards: Card[] }) => {
   return (
     <div className="flex flex-col gap-4 my-8">
       <Input placeholder="Search Card" className="max-w-lg" />
-      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
-        {cards.map((card: Card) => (
-          <PokemonCard
-            key={card.id}
-            imageUrl={card.images.small}
-            caption={card.name}
-            number={card.number}
-            price={
-              card.tcgplayer.prices?.holofoil?.market ||
-              card.tcgplayer.prices?.reverseHolofoil?.market
-            }
-            onClick={() => {
-              console.log("Card clicked");
-            }}
-          />
-        ))}
-      </div>
+      <ul className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
+        <AnimatePresence custom="exit">
+          {cards.map((card, index) => (
+            <motion.li
+              key={card.id}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={{
+                hidden: { opacity: 0, y: -10 },
+                visible: (index) => ({
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    delay: index * 0.1,
+                    duration: 0.4,
+                    ease: "easeOut",
+                  },
+                }),
+                exit: {
+                  opacity: 0,
+                  y: -10,
+                  transition: { duration: 0.1 },
+                },
+              }}
+              custom={index}
+            >
+              <PokemonCard
+                imageUrl={card.images.small}
+                caption={card.name}
+                number={card.number}
+                price={
+                  card.tcgplayer.prices?.holofoil?.market ||
+                  card.tcgplayer.prices?.reverseHolofoil?.market
+                }
+                onClick={() => {
+                  console.log("Card clicked");
+                }}
+              />
+            </motion.li>
+          ))}
+        </AnimatePresence>
+      </ul>
     </div>
   );
 };
